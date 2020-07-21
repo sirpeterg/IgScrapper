49bdfcbeccc7dca529be8521200d576be65b710b
sirpeterg <sir-pepe@hotmail.com>
2020-07-21 16:44:14 +0200

    Minor refractoring, adding docstrings to all methods

--- main.py
+++ main.py
@@ -1,27 +1,9 @@
 # Github user: sirpeterg
-# date: 2020-06-23
+# date: 2020-07-21
 # version 0.3
 # changes: Switched from
 # functional to object oriented design
 
-##################################
-#HIGH LEVEL
-##################################
-#while not EndConditionReached:
-
-    # 0) check the status of the DB, and make a decision what to do, based on the status
-        # if enough ulrs are in DB that are not downloaded, grab some of them to the DB
-        # else, get new post urls into DB (using UserList.txt, a file with names of users to scrap)
-
-    # 1) get list of posts from random user, from the DB
-
-    # 2.1) download the image
-
-    # 2.2) if image was saved get the metadata (likes etc, statistics of user) and write to DB
-
-    # Do pause
-
-##################################
 
 import instaloader
 from selenium import webdriver
@@ -33,7 +15,6 @@ import datetime
 import os
 import os.path
 from os import path
-import re
 
 #####################
 def pause(size):
@@ -48,7 +29,7 @@ def pause(size):
     elif size == 'M':
         pauselength = 50.7 * (random() + 0.1)  # between 5 sec and 1 min
     elif size == 'L':
-        pauselength = 1.6 * 151.3 * (random() + 0.25)  # between 1.6 *( 37 sec and 3.15 min)
+        pauselength = 1.3 * 151.3 * (random() + 0.25)  # between 1.3 *( 37 sec and 3.15 min)
     elif size == 'XL':
         pauselength = 141.3 * (random() + 0.21) + 272  # between 5.02 and 7.38 min
     elif size == 'XXL':
@@ -56,12 +37,6 @@ def pause(size):
 
     print('|    Pausing for: ', round(pauselength / 60.0, 1), ' minutes')
     time.sleep(pauselength)
-
-
-def merge(list1, list2):
-    """Merges two list into a list of tuples"""
-    merged_list = [(list1[i], list2[i]) for i in range(0, len(list1))]
-    return merged_list
 
 #####################
 
@@ -187,11 +162,6 @@ class Database:
         self.cursor.execute('''SELECT * FROM photo WHERE is_downloaded == "False";''')
         return self.cursor.fetchall()
 
-    #def getNonDownloadedPosts_userNames(self):
-    #    """Gets all photographer_names from posts in the DB that have not yet been downloaded"""
-    #   self.cursor.execute('''SELECT photographer_name FROM photo WHERE is_downloaded == "False";''')
-    #    return self.cursor.fetchall()
-
     def getNumberPosts(self):
         """Gets number of all posts from DB"""
         self.cursor.execute('''SELECT * FROM photo;''')
@@ -211,6 +181,7 @@ class Database:
         return NumDownloaded
 
     def writePictureSavedFlag(self, userpost):
+        """Updates database, flags that this post jpg has been saved"""
         with self.connection:
             # identify the DB ID of the post url
             self.cursor.execute("SELECT photo_id FROM photo WHERE photo_url = ? ", (userpost.getPostUrl(),))
@@ -219,20 +190,24 @@ class Database:
             #print("|    Updated the saving event to the database")
 
     def identifyIfUrlIsInDatabase(self, postUrl):
+        """Checks if this post is already in the database by comparing its URL to all URLs saved in the database"""
         self.cursor.execute("SELECT photo_url FROM photo WHERE photo_url = (?);", (postUrl,))
         similar_link = self.cursor.fetchall()
         return len(similar_link) != 0
 
     def removePostFromDb(self, userpost):
+        """Deletes a post from the database, by comparing its URL to all URLs saved in the ddatabase"""
         with self.connection:
             self.cursor.execute("DELETE FROM photo WHERE photo_url = (?);", (userpost.getPostUrl(),))
 
     def printStatus(self):
+        """Prints statistics on how many posts are in the database"""
         print("|    Number of posts in DB: ", self.getNumberPosts())
         print("|    Number of posts to download: ", self.getNumberNonDownloadedPosts())
         print("|    Number of posts downloaded: ",  self.getNumberDownloadedPosts())
 
 class Userprofile:
+    """A class representing the page of an instagram user"""
     def __init__(self, homePath, databasePath, UserListPath):
         self.homePath = homePath
         self.driver = None
@@ -244,16 +219,23 @@ class Userprofile:
         self.postUrls = []
 
     def visitProfile(self):
-        print("|    Profile name: ",self.userName)
+        """Creates a webdriver and visits the url of the instagram user"""
+        print("|    Scrapping posts to database from user: ", self.userName)
         self.driver = RandomDriver()
         self.driver.get(self.userUrl)
 
+    def closeDriver(self):
+        """Closes the webdriver"""
+        self.driver.quit()
+
     def scroll(self):
+        """Scrolls downwards"""
         scroll_down = "window.scrollTo(0, document.body.scrollHeight);"
         self.driver.execute_script(scroll_down)
         pause('XXS')
 
     def getListOfPresentPosts(self):
+        """Scraps list of urls that refer to (visible) pictures of this instagram user on their profile (needs the webdrive)"""
         try:
             links = [a.get_attribute('href') for a in self.driver.find_elements_by_tag_name('a')]
         except:
@@ -266,6 +248,8 @@ class Userprofile:
         return(postList)
 
     def __getRandomUser(self):
+        """Helper function: gets a random user from the UserList.txt file
+        (format of UserList.txt: /username1/\n/username2/\n etc  ) """
         try:
             with open(self.UserListPath, 'r') as openedFile: # open txt file
                 allUsers = openedFile.readlines()
@@ -278,6 +262,7 @@ class Userprofile:
         self.userName = randomUsername
 
     def isolateUniquePostFromList(self, postList):
+        """returns all post urls that are not already in the database"""
         postListUnique = []
         with Database(self.databasePath) as db:
             for postUrl in postList:
@@ -286,10 +271,11 @@ class Userprofile:
         return postListUnique
 
     def getName(self):
+        """returns the name of the instagram user"""
         return self.userName
 
 class Userpost:
-
+    """Class that represents a single post (picture) on Instagram"""
     def __init__(self, homePath, DatabaseFolder,  postUrl, userName):
         self.homePath = homePath
         self.driver = None
@@ -312,6 +298,11 @@ class Userpost:
                         }
 
     def __startInstaloaderInstance(self):
+        """Helper function that creates an instance of the Instaloader class.
+        Required for methods:
+            downloadPic(self):
+            downloadMetadata(self):
+        """
         shortcode = self.metadata['photo_url'].split('/')[-2]
         a = self.DatabaseFolder[:] # make copy, don't mess with the original
         db_folder = a.replace(self.homePath, '')
@@ -328,6 +319,10 @@ class Userpost:
         return L
 
     def downloadPic(self):
+        """Downloads the jpg of this post (or all jpgs, if multiimage)
+        to a folder named by the Instagram User
+        Returns a True flag if image was saved
+        Since this flag is not 100% reliable, this is complemented by the confirmPresenceOfJpg(self) method"""
         L = self.__startInstaloaderInstance()
         shortcode = self.metadata['photo_url'].split('/')[-2]
         profile = Profile.from_username(L.context, self.metadata['photographer_name'])
@@ -341,6 +336,12 @@ class Userpost:
         return picture_save_flag
 
     def confirmPresenceOfJpg(self):
+        """ Checks if the jpg belonging to this post is present
+        This method will not work for multiimages, since it
+        checks for shortcode + '.jpg', while multiimages are called
+        shortcode + '_1.jpg', shortcode + '_2.jpg' etc
+        RUN THIS AFTER removeTrailingMulitimages(self) !!!!
+        """
         shortcode = self.metadata['photo_url'].split('/')[-2]
         path = self.DatabaseFolder + self.metadata['photographer_name']
         isInDB = os.path.isfile(path + '/' + shortcode + '.jpg')
@@ -350,6 +351,7 @@ class Userpost:
 
 
     def downloadMetadata(self):
+        """Downloads metadata of this post to the database"""
         L = self.__startInstaloaderInstance()
         shortcode = self.metadata['photo_url'].split('/')[-2]
         profile = Profile.from_username(L.context, self.metadata['photographer_name'])
@@ -395,12 +397,20 @@ class Userpost:
         return metadata
 
     def getUsername(self):
+        """Returns Instagram user name of the photographer from this post"""
         return self.metadata['photographer_name']
 
     def getPostUrl(self):
+        """Returns the url of this post"""
         return self.metadata['photo_url']
 
-    def removeTrailingMulitimages(self): # does not modify the jpg of a 'single-image-post"
+    def removeTrailingMulitimages(self):
+        """Finds all jpgs belonging to a single post.
+        Then it deletes all but the first image that is the covershot of the post
+        This jpg is then renamed from shortcode + '_1.jpg' to shortcode + '.jpg'
+        NOTE: This is a neutral action for 'single-image-post', and it can run over these
+        as well, without compromising the file and filename
+        """
         jpgpath = self.DatabaseFolder + self.metadata['photographer_name'] + '/'
         files = []
         shortcode = self.metadata['photo_url'].split('/')[-2]
@@ -422,17 +432,25 @@ class Userpost:
                     os.remove(jpgpath + '/' + files[i])  # delete all but the first multiimage
 
 class ScrappApp:
-
+    """Class representing the Instagram Scrapper"""
     def __init__(self, homePath, databasePath, databaseFolder,  UserListPath):
         self.homePath = homePath
         self.databasePath = databasePath
         self.UserListPath = UserListPath
         self.databaseFolder = databaseFolder
         self.startScrapping = datetime.datetime.now()
-        self.maxRuntime = 10
-        self.minimumNumUrlsPresent = 9
+        self.maxRuntime = 1600 # duration of scrap run in minutes
+        self.minimumNumUrlsPresent = 9 # if less than this number of database entries are present that
+        # are not scrapped for metadata and the jpg, the script feeds the database with fresh,
+        # non-scrapped post entries
 
     def scrapUniquePostsToDb(self, userprofile):
+        """Requires a webdriver that is at the homepage of an Instagram user
+        Gets a list of all post urls that are not yet in database
+        Then creates a userpost instance for each
+        Then writes all posts (not the metadata, only url and photographer name) to the database
+        To complete the metadata, use: userpost.downloadMetadata(self)
+        To download the jpg, use: userpost.downloadPic(self)"""
         photographerName = userprofile.getName()
         postList = userprofile.getListOfPresentPosts()
         urlListUnique = userprofile.isolateUniquePostFromList(postList)
@@ -442,7 +460,14 @@ class ScrappApp:
                 db.createPostEntryInDatabase(p)
 
     def getUrlListUnique(self):
-        """Gets a list of Instagram post urls that are not yet in the database"""
+        """Applies scrapUniquePostsToDb(self, userprofile) until the required amount (self.minimumNumUrlsPresent) of
+        posts that are not yet scrapped are in the database
+        SCRIPT:
+        go to user homepage
+        If not enough new posts (not in Database already) are present:
+            scroll a random number of times down and check again
+        If not enough new posts (not in Database already) are present:
+        Start again, with a different user until enough fresh posts are present"""
         with Database(self.databasePath) as db:
             NumberNonDownloadedPosts = db.getNumberNonDownloadedPosts()
         while NumberNonDownloadedPosts < self.minimumNumUrlsPresent: # are there enough posts to download?
@@ -462,11 +487,14 @@ class ScrappApp:
                         userprofile.scroll()
                         self.scrapUniquePostsToDb(userprofile)
         print("|    sufficient posts")
+        self.closeDriver()
         with Database(self.databasePath) as db:
             MetadataNonDownloadedPosts = db.getMetadataNonDownloadedPosts()
         return MetadataNonDownloadedPosts
 
     def endConditionReached(self):
+        """The Scrapper terminates if it runs longer than self.maxRuntime (in minutes)
+        returns True if the scrapper should terminate"""
         duration_run = datetime.datetime.now() - self.startScrapping
         duration_run_minutes = round(duration_run.total_seconds() / 60, 2)
         EndconditionReached = duration_run_minutes > self.maxRuntime
@@ -476,7 +504,20 @@ class ScrappApp:
         return EndconditionReached
 
     def run(self):
-
+        """
+        ##################################
+        #HIGH LEVEL
+        ##################################
+        #while not EndConditionReached:
+            # 0) check the status of the DB, and make a decision what to do, based on the status
+                # if enough ulrs are in DB that are not downloaded, grab some of them to the DB
+                # else, get new post urls into DB (using UserList.txt, a file with names of users to scrap)
+            # 1) get list of posts from random user, from the DB
+            # 2.1) download the image
+            # 2.2) if image was saved get the metadata (likes etc, statistics of user) and write to DB
+            # Do pause
+        ##################################
+        """
         while not self.endConditionReached():
             print("")
             print("_____ MODULE 1__________")
@@ -499,18 +540,20 @@ class ScrappApp:
             print("| Database status before")
             with Database(self.databasePath) as db:
                 db.printStatus()
-            print("saving Post: ", userpost.getPostUrl(), " (Username: ", userpost.getUsername(),")")
             #userpost = Userpost(self.homePath, 'https://www.instagram.com/p/CAqQ0QsAZZW/', 'danielkordan')
+            #userpost = Userpost(self.homePath, self.databaseFolder, 'https://www.instagram.com/p/CCa9o7LJb6Y/', 'akulka174')
+            print("|    saving Post: ", userpost.getPostUrl(), " (Username: ", userpost.getUsername(), ")")
             saveSuccesful = userpost.downloadPic()
+            if saveSuccesful:
+                userpost.removeTrailingMulitimages()
+            JPEGpresent = userpost.confirmPresenceOfJpg()
             print("|    Saving succesful: ", saveSuccesful)
+            print("|    Post is video?: ", userpost.postIsVideo)
             if userpost.postIsVideo:
                 print('|    post could not be saved, it was a video')
-            if saveSuccesful:
+            if (saveSuccesful and JPEGpresent):
                 with Database(self.databasePath) as db:
                     db.writePictureSavedFlag(userpost)
-                userpost.removeTrailingMulitimages()
-                JPEGpresent = userpost.confirmPresenceOfJpg()
-            #pause("S")
             print("__")
             print("")
             ####################
@@ -518,14 +561,16 @@ class ScrappApp:
             print("VISIT RANDOM PHOTO URL (scrap metadata)")
             print("_____ MODULE 2.2__________")
             print("|")
-            if saveSuccesful and JPEGpresent:
+            if (saveSuccesful and JPEGpresent):
                 userpost.downloadMetadata()
                 with Database(self.databasePath) as db:
                     db.writeMetadataToDb(userpost)
-            if userpost.postIsVideo: # remove from DB
+            else:
+                print("|    No metadata saved, since jpg was not saved")
+            if (userpost.postIsVideo or not(JPEGpresent)) == True: # remove from DB
                 with Database(self.databasePath) as db:
                     db.removePostFromDb(userpost)
-                print("|    will remove post from DB, is Video")
+                print("|    will remove post from DB, is Video or not downloadable")
             print("| Database status after")
             with Database(self.databasePath) as db:
                 db.printStatus()
@@ -535,20 +580,22 @@ class ScrappApp:
             pause('L')
             if random() < 0.12:
                 pause('L')
-
-
         print("DONE")
 
 
 
+def main():
+    homePath = '/Users/Peterg/code/IgScrapper'
+    databaseFolder = homePath + '/Database_img/'
+    databasePath = databaseFolder + 'IGdata.db'
+    UserListPath = homePath + '/UserList.txt'
+    # UserList.txt has the instagram profiles to scrap:
+    # format:
+    #/username1/\n/username2/\n/username3/ etc (\n represents newline)
+    scrapper = ScrappApp(homePath, databasePath, databaseFolder, UserListPath)
+    scrapper.run()
 
-homePath = '/Users/Peterg/code/IgScrapper'
-databaseFolder = homePath + '/Database_img/'
-databasePath = databaseFolder + 'IGdata.db'
-UserListPath = homePath + '/UserList.txt'
-scrapper = ScrappApp(homePath, databasePath, databaseFolder, UserListPath)
-scrapper.run()
-
+if __name__ == '__main__': main()
 
 
 
